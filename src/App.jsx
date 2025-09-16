1411232cc9bfa69e7a2ff60d0f4f1fb4071986a6
Joseph Milliken <imjmilli21@gmail.com>
2025-09-15 22:59:18 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">🚀 Tailwind is working!</h1>
+    </div>
+  );
+}
